0d64096eec7a8738acf7babd9c9a3f8b20ee05e8
abhishekrathour27 <abhishekrathour96081@gmail.com>
2025-05-08 11:30:30 +0530

    fix logo size in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
             <div className='md:flex md:flex-col  md:gap-4 h-full pl-10 lg:w-1/2 mt-5'>
                 <h1 className='text-4xl font-semibold lg:w-96'>Book Appointment With Trusted Doctors</h1>
                 <div className='lg:flex lg:w-[40vw] gap-4'>
-                    <img src="/group_profiles.png" alt="group_profile" className='lg:w-28' />
+                    <img src="/group_profiles.png" alt="group_profile" className='w-[100px] lg:w-28' />
                     <p>Simply browse through our extensive list of trusted doctors,
                         schedule your appointment hassle-free.</p>
                 </div>
